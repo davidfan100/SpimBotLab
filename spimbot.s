839158819a6b276d59d037103df1387c487368e7
David Fan <davidf3@illioni.edu>
2018-12-09 17:12:55 -0600

    added sol of rule1 but still doesn't work

--- spimbot.s
+++ spimbot.s
@@ -4,6 +4,7 @@ PRINT_STRING            = 4
 PRINT_CHAR              = 11
 PRINT_INT               = 1
 
+
 # memory-mapped I/O
 VELOCITY                = 0xffff0010
 ANGLE                   = 0xffff0014
@@ -45,8 +46,8 @@ REQUEST_PUZZLE_ACK      = 0xffff00d8
 #    struct spim_treasure treasures[50];
 #};
 .data
-sudoku:       .word 128                       # puzzle is stored as tree-based array
-puzzle_res:     .word 1                             # the solution to the puzzle
+sudoku:       .word 1                       # puzzle is stored as tree-based array
+puzzle_res:     .space 1                             # the solution to the puzzle
 puzzle_start:   .word 1                             # boolean flag to tell us when to start requesting puzzles
 treasure_map:   .word 0:404                         # treasure map array, each treasure has x and y location, and point value
 
@@ -108,8 +109,8 @@ solve_puzzle: # function to solve a puzzle (must have requested a puzzle first)
         # sub       $sp, $sp, 8
         # sw        $ra, 0($sp)
         # sw        $s0, 4($sp)
-        la        $a0, sudoku
-        jal       rule1
+        la          $a0, sudoku
+        jal         rule1
         # move      $s0, $v0
         # la        $a0, sudoku
         # jal       rule2
@@ -122,7 +123,7 @@ solve_puzzle: # function to solve a puzzle (must have requested a puzzle first)
         # lw        $ra, 0($sp)
         # lw        $s0, 4($sp)
         # add       $sp, $sp, 8
-        j         req_puzzle
+        j         infinite
 req_puzzle: # function to request a puzzle
         la        $t2, sudoku
         sw        $t2, REQUEST_PUZZLE($0)
@@ -142,8 +143,6 @@ load_treasure_map: # get the treasure_map struct
 pick_treasure: # function to pick up treasure
         sw        $0, PICK_TREASURE($0)
         j         infinite
-
-
 
 
 has_single_bit_set:
@@ -169,180 +168,132 @@ get_square_begin:
 ##      Rule 1 Implementation
 ##
 ##
+board_address:
+	mul	$v0, $a1, 16		# i*16
+	add	$v0, $v0, $a2		# (i*16)+j
+	sll	$v0, $v0, 1		# ((i*9)+j)*2
+	add	$v0, $a0, $v0
+	jr	$ra
+
+.globl rule1
 rule1:
-	sub $sp, $sp, 32
-	sw $ra, 0($sp)
-	sw $s0, 4($sp) ## changed
-	sw $s1, 8($sp) ## i
-	sw $s2, 12($sp) ## j
-	sw $s3, 16($sp) ## board
-	sw $s4, 20($sp) ## board[i][j]
-	sw $s5, 24($sp) ## ii
-	sw $s6, 28($sp) ## jj
+	sub	$sp, $sp, 32 		
+	sw	$ra, 0($sp)		# save $ra and free up 7 $s registers for
+	sw	$s0, 4($sp)		# i
+	sw	$s1, 8($sp)		# j
+	sw	$s2, 12($sp)		# board
+	sw	$s3, 16($sp)		# value
+	sw	$s4, 20($sp)		# k
+	sw	$s5, 24($sp)		# changed
+	sw	$s6, 28($sp)		# temp
+	move	$s2, $a0		# store the board base address
+	li	$s5, 0			# changed = false
 
-	move $s3, $a0
-	li $t0, 0 # changed
-	move $s0, $t0
-	li $t1, 0 # i
-loop_rule_1:
-	li $t2, 0 # j
+	li	$s0, 0			# i = 0
+r1_loop1:
+	li	$s1, 0			# j = 0
+r1_loop2:
+	move	$a0, $s2		# board
+	move 	$a1, $s0		# i
+	move	$a2, $s1		# j
+	jal	board_address
+	lhu	$s3, 0($v0)		# value = board[i][j]
+	move	$a0, $s3		
+	jal	has_single_bit_set
+	beq	$v0, 0, r1_loop2_bot	# if not a singleton, we can go onto the next iteration
 
-	blt $t1, 16, loop_rule_2
-	move $v0, $s0
-	j end
-
-loop_rule_2:
-	move $s1, $t1
-	move $s2, $t2
-
-	mul $t3, $s1, 16
-	add $t3, $t3, $s2
-	mul $t3, $t3, 2
-	add $t3, $s3, $t3
-	lhu $a0, 0($t3)
-
-	move $s4, $a0 ## moving value board[i][j] to the argument reg
-
-	jal has_single_bit_set
-
-	li $t1, 0 # store values of k
-	beq $v0, 1, loop_for_has_bit_1
-
-	j for_next_rule_loop
+	li	$s4, 0			# k = 0
+r1_loop3:
+	beq	$s4, $s1, r1_skip_row	# skip if (k == j)
+	move	$a0, $s2		# board
+	move 	$a1, $s0		# i
+	move	$a2, $s4		# k
+	jal	board_address
+	lhu	$t0, 0($v0)		# board[i][k]
+	and	$t1, $t0, $s3		
+	beq	$t1, 0, r1_skip_row
+	not	$t1, $s3
+	and	$t1, $t0, $t1		
+	sh	$t1, 0($v0)		# board[i][k] = board[i][k] & ~value
+	li	$s5, 1			# changed = true
 	
-for_next_rule_loop:
-	add $t2, $s2, 1
-	move $t1, $s1
-	blt $t2, 16, loop_rule_2
+r1_skip_row:
+	beq	$s4, $s0, r1_skip_col	# skip if (k == i)
+	move	$a0, $s2		# board
+	move 	$a1, $s4		# k
+	move	$a2, $s1		# j
+	jal	board_address
+	lhu	$t0, 0($v0)		# board[k][j]
+	and	$t1, $t0, $s3		
+	beq	$t1, 0, r1_skip_col
+	not	$t1, $s3
+	and	$t1, $t0, $t1		
+	sh	$t1, 0($v0)		# board[k][j] = board[k][j] & ~value
+	li	$s5, 1			# changed = true
 
-	add $t1, $s1, 1
+r1_skip_col:	
+	add	$s4, $s4, 1		# k ++
+	blt	$s4, 16, r1_loop3
 
-	j loop_rule_1
+	## doubly nested loop
+	move	$a0, $s0		# i
+	jal	get_square_begin
+	move	$s6, $v0		# ii
+	move	$a0, $s1		# j
+	jal	get_square_begin	# jj
 
-loop_for_has_bit_1: 
-	bge $t1, 16, loop_for_has_bit_2_prep
-	bne $t1, $s2, if_row 
-	bne $t1, $s1, if_column
+	move 	$t0, $s6		# k = ii
+	add	$t1, $t0, 4		# ii + GRIDSIZE
+	add 	$s6, $v0, 4		# jj + GRIDSIZE
 
-	add $t1, $t1, 1
+r1_loop4_outer:
+	sub	$t2, $s6, 4		# l = jj  (= jj + GRIDSIZE - GRIDSIZE)
 
-	j loop_for_has_bit_1
+r1_loop4_inner:
+	bne	$t0, $s0, r1_loop4_1
+	beq	$t2, $s1, r1_loop4_bot
 
-if_row:
-	mul $t2, $s1, 16
-	add $t2, $t2, $t1
-	mul $t2, $t2, 2
-	add $t2, $s3, $t2
-	lhu $t3, 0($t2)   #obtaining board[i][k] value
+r1_loop4_1:	
+	mul	$v0, $t0, 16		# k*16
+	add	$v0, $v0, $t2		# (k*16)+l
+	sll	$v0, $v0, 1		# ((k*16)+l)*2
+	add	$v0, $s2, $v0		# &board[k][l]
+	lhu	$v1, 0($v0)		# board[k][l]
+   	and	$t3, $v1, $s3		# board[k][l] & value
+	beq	$t3, 0, r1_loop4_bot
 
-	and $t4, $t3, $s4
-	bne $t4, 0, if_row_changing
-	bne $t1, $s1, if_column
+	not	$t3, $s3
+	and	$v1, $v1, $t3		
+	sh	$v1, 0($v0)		# board[k][l] = board[k][l] & ~value
+	li	$s5, 1			# changed = true
 
-	add $t1, $t1, 1
-	j loop_for_has_bit_1
+r1_loop4_bot:	
+	add	$t2, $t2, 1		# l++
+	blt	$t2, $s6, r1_loop4_inner
 
-if_row_changing:
-	not $t4, $s4
-
-	and $t5, $t3, $t4
-	sh $t5, 0($t2)
-	li $s0, 1
-
-	bne $t1, $s1, if_column
-	add $t1, $t1, 1
-
-	j loop_for_has_bit_1
+	add	$t0, $t0, 1		# k++
+	blt	$t0, $t1, r1_loop4_outer
 	
-if_column:
-	mul $t2, $t1, 16
-	add $t2, $t2, $s2
-	mul $t2, $t2, 2
-	add $t2, $s3, $t2
-	lhu $t3, 0($t2) #obtaining board[k][j]
 
-	and $t4, $t3, $s4
-	bne $t4, 0, if_column_changing
-	add $t1, $t1, 1
-	j loop_for_has_bit_1
+r1_loop2_bot:	
+	add	$s1, $s1, 1		# j ++
+	blt	$s1, 16, r1_loop2
 
-if_column_changing:
-	not $t4, $s4
+	add	$s0, $s0, 1		# i ++
+	blt	$s0, 16, r1_loop1
 
-	and $t5, $t3, $t4
-	sh $t5, 0($t2)
-	li $s0, 1
+	move	$v0, $s5		# return changed
+	lw	$ra, 0($sp)		# restore registers and return
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	lw	$s2, 12($sp)
+	lw	$s3, 16($sp)
+	lw	$s4, 20($sp)
+	lw	$s5, 24($sp)
+	lw	$s6, 28($sp)
+	add	$sp, $sp, 32
+	jr	$ra
 
-	add $t1, $t1, 1
-	j loop_for_has_bit_1
-
-loop_for_has_bit_2_prep:
-	move $a0, $s1
-	jal get_square_begin
-	
-	move $s5, $v0
-	move $a0, $s2
-
-	jal get_square_begin
-
-	move $s6, $v0
-
-	move $t0, $s5 ## k
-	add $t2, $s5, 4## ii + 4 //because the size of each square is 4 x 4
-	add $t3, $s6, 4 ## jj+ 4
-	j loop_for_has_bit_2_k
-
-loop_for_has_bit_2_k:
-	bge $t0, $t2, for_next_rule_loop
-	move $t1, $s6 ## l
-
-loop_for_has_bit_2_l:
-	seq $t4, $t0, $s1
-	seq $t5, $t1, $s2
-
-	and $t6, $t4, $t5
-
-	bne $t6, 0, for_next_rule_loop_bit
-
-	mul $t4, $t0, 16
-	add $t4, $t4, $t1
-	mul $t4, $t4, 2
-	add $t4, $t4, $s3
-	lhu $t5, 0($t4)
-
-	and $t6, $t5, $s4
-	bne $t6, 0, if_square_changing
-
-	j for_next_rule_loop_bit
-
-if_square_changing:
-	not $t6, $s4
-
-	and $t6, $t5, $t6
-	sh $t6, 0($t4)
-
-	j for_next_rule_loop_bit
-
-for_next_rule_loop_bit:
-	add $t1, $t1, 1
-	
-	blt $t1, $t3, loop_for_has_bit_2_l
-
-	add $t0, $t0, 1
-	j loop_for_has_bit_2_k
-
-end:
-	lw $s6, 28($sp) # get_square_begin(j)
-	lw $s5, 24($sp) # get_square_begin(i)
-	lw $s4, 20($sp) ## board[i][j]
-	lw $s3, 16($sp) ## board
-	lw $s2, 12($sp) ## j
-	lw $s1, 8($sp) ## i
-	lw $s0, 4($sp) ## changed
-	lw $ra, 0($sp)
-	add $sp, $sp, 32
-	
-	jr $ra
 
 ##
 ##
